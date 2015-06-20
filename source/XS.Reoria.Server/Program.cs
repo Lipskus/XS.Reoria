be8a686638aabb184221ab6e6710fb8c63010959
Jonathan Campbell <jonathan.campbell.1992@hotmail.co.uk>
2015-06-20 03:09:36 -0400

    Created basic debug application for server.

---
+++ source/XS.Reoria.Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XS.Reoria.Server
+{
+    class Program
+    {
+        static bool run = true;
+        static void Main(string[] args)
+        {
+            Thread input = new Thread(() => {
+                if ((Console.ReadLine() == "exit") || (Console.ReadLine() == "stop"))
+                {
+                    run = false;
+                }
+            });
+
+            input.Start();
+
+            while (run)
+            {
+                Console.WriteLine("Hello!");
+            }
+
+            input = null;
+        }
+    }
+}
